04079d982b6b664f377e492ebe9587d0b11b63eb
maxizavala <maximiliano.zavala@davinci.edu.ar>
2023-04-18 16:44:43 -0300

    GET findById

--- src/main/java/com/deg/restTemplate/controller/UserController.java
+++ src/main/java/com/deg/restTemplate/controller/UserController.java
@@ -4,17 +4,27 @@ import com.deg.restTemplate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/users")
 public class UserController {
+
     @Autowired
     private UserService userService;
+
     @GetMapping("/")
     public ResponseEntity<?> getUsers() {
         return new ResponseEntity<>(this.userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(this.userService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createPost() {
+        return new ResponseEntity<>(this.userService.createPost(), HttpStatus.OK);
     }
 }
--- src/main/java/com/deg/restTemplate/service/UserService.java
+++ src/main/java/com/deg/restTemplate/service/UserService.java
@@ -3,12 +3,26 @@ package com.deg.restTemplate.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
+    RestTemplate restTemplate = new RestTemplate();
     public List<String> findAll(){
-        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users", List.class);
+    }
+
+    public String findById(Long id){
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users/"+id, String.class);
+    }
+
+    public String createPost(){
+        Map<String, String> bodyMap = new HashMap<>();
+        bodyMap.put("title", "HOLA MUNDO");
+        bodyMap.put("body", "Mensaje de prueba");
+        bodyMap.put("userId", "1");
+        return restTemplate.postForObject("https://jsonplaceholder.typicode.com/posts", null, String.class, bodyMap);
     }
 }
